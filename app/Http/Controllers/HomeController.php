c7953e70c81d395a6ad5fc9c8e55803accaf6340
Megumi Minagawa <3923gajt@gmail.com>
2021-08-15 15:32:30 +0900

    テスト前初期登録

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Comment;
+use App\Models\prefecture;
+use App\Models\babyage_scope;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(){
+
+          
+        if(isset($_GET['pref'],$_GET['babyage'],$_GET['year'],$_GET['month'])){
+            //  dd($_GET['pref']);
+            //都道府県・子供の年齢・年を選択・月を選択が全部選択されていない場合
+            if($_GET['pref'][0] === "都道府県を選ぶ" && $_GET['babyage'][0] === "年齢を選ぶ" && $_GET['year'][0] === "年を選択" && $_GET['month'][0] === "月を選択"){
+                $posts=Post::orderBy('created_at','desc')->get();
+            }
+
+            //都道府県（都道府県を選ぶは含まない）・子供の年齢（子供の年齢を選ぶは含まない）・年を選択・月を選択が全部選択されている
+            if($_GET['pref'][0] !== "都道府県を選ぶ" && $_GET['babyage'][0] !== "年齢を選ぶ" && $_GET['year'][0] !== "年を選択" && $_GET['month'][0] !== "月を選択"){
+                $posts=Post::whereIn('prefecture_id',$_GET['pref'])->whereIn('babyage_scope_id',$_GET['babyage'])->whereIn('year',$_GET['year'])->whereIn('month',$_GET['month'])->get();
+            }
+
+            //都道府県（都道府県を選ぶは含まない）のみ選択されている
+            if($_GET['pref'][0] !== "都道府県を選ぶ" && $_GET['babyage'][0] === "年齢を選ぶ" && $_GET['year'][0] === "年を選択" && $_GET['month'][0] === "月を選択"){
+                // dd($_GET['pref']);
+                $posts=Post::whereIn('prefecture_id',$_GET['pref'])->get();
+            }
+
+            //子供の年齢（子供の年齢を選ぶは含まない）のみ選択されている
+            if($_GET['pref'][0] === "都道府県を選ぶ" && $_GET['babyage'][0] !== "年齢を選ぶ" && $_GET['year'][0] === "年を選択" && $_GET['month'][0] === "月を選択"){
+                $posts=Post::whereIn('babyage_scope_id',$_GET['babyage'])->get();
+            }
+
+            //年を選択のみ選択されている
+              if($_GET['pref'][0] === "都道府県を選ぶ" && $_GET['babyage'][0] === "年齢を選ぶ" && $_GET['year'][0] !== "年を選択" && $_GET['month'][0] === "月を選択"){
+                $posts=Post::whereIn('year',$_GET['year'])->get();
+            }
+
+            //月を選択のみ選択されている
+              if($_GET['pref'][0] === "都道府県を選ぶ" && $_GET['babyage'][0] === "年齢を選ぶ" && $_GET['year'][0] === "年を選択" && $_GET['month'][0] !== "月を選択"){
+                $posts=Post::whereIn('month',$_GET['month'])->get();
+            }
+
+            //都道府県以外選択されている
+            if($_GET['pref'][0] === "都道府県を選ぶ" && $_GET['babyage'][0] !== "年齢を選ぶ" && $_GET['year'][0] !== "年を選択" && $_GET['month'][0] !== "月を選択"){
+                $posts=Post::whereIn('babyage_scope_id',$_GET['babyage'])->whereIn('year',$_GET['year'])->whereIn('month',$_GET['month'])->get();
+            }
+
+            //子供の年齢以外選択されている
+            if($_GET['pref'][0] !== "都道府県を選ぶ" && $_GET['babyage'][0] === "年齢を選ぶ" && $_GET['year'][0] !== "年を選択" && $_GET['month'][0] !== "月を選択"){
+                $posts=Post::whereIn('prefecture_id',$_GET['pref'])->whereIn('year',$_GET['year'])->whereIn('month',$_GET['month'])->get();
+            }
+
+            //年の選択以外選択されている
+            if($_GET['pref'][0] !== "都道府県を選ぶ" && $_GET['babyage'][0] !== "年齢を選ぶ" && $_GET['year'][0] === "年を選択" && $_GET['month'][0] !== "月を選択"){
+                $posts=Post::whereIn('prefecture_id',$_GET['pref'])->whereIn('babyage_scope_id',$_GET['babyage'])->whereIn('month',$_GET['month'])->get();
+            }
+
+             //月の選択以外選択されている
+             if($_GET['pref'][0] !== "都道府県を選ぶ" && $_GET['babyage'][0] !== "年齢を選ぶ" && $_GET['year'][0] !== "年を選択" && $_GET['month'][0] === "月を選択"){
+                $posts=Post::whereIn('prefecture_id',$_GET['pref'])->whereIn('babyage_scope_id',$_GET['babyage'])->where('year',$_GET['year'])->get();
+            }
+
+             //都道府県・子供の年齢のみ選択されている
+             if($_GET['pref'][0] !== "都道府県を選ぶ" && $_GET['babyage'][0] !== "年齢を選ぶ" && $_GET['year'][0] === "年を選択" && $_GET['month'][0] === "月を選択"){
+                $posts=Post::whereIn('prefecture_id',$_GET['pref'])->whereIn('babyage_scope_id',$_GET['babyage'])->get();
+            }
+
+              //都道府県・年の選択のみ選択されている
+              if($_GET['pref'][0] !== "都道府県を選ぶ" && $_GET['babyage'][0] === "年齢を選ぶ" && $_GET['year'][0] !== "年を選択" && $_GET['month'][0] === "月を選択"){
+                $posts=Post::whereIn('prefecture_id',$_GET['pref'])->whereIn('year',$_GET['year'])->get();
+            }
+
+              //都道府県・月の選択のみ選択されている
+              if($_GET['pref'][0] !== "都道府県を選ぶ" && $_GET['babyage'][0] === "年齢を選ぶ" && $_GET['year'][0] === "年を選択" && $_GET['month'][0] !== "月を選択"){
+                $posts=Post::whereIn('prefecture_id',$_GET['pref'])->whereIn('month',$_GET['month'])->get();
+            }
+
+            //年・月を選択のみ選択されている
+            if($_GET['pref'][0] === "都道府県を選ぶ" && $_GET['babyage'][0] === "年齢を選ぶ" && $_GET['year'][0] !== "年を選択" && $_GET['month'][0] !== "月を選択"){
+                $posts=Post::whereIn('year',$_GET['year'])->whereIn('month',$_GET['month'])->get();
+            }
+
+             //子供の年齢・年を選択のみ選択されている
+             if($_GET['pref'][0] === "都道府県を選ぶ" && $_GET['babyage'][0] !== "年齢を選ぶ" && $_GET['year'][0] !== "年を選択" && $_GET['month'][0] === "月を選択"){
+                $posts=Post::whereIn('babyage_scope_id',$_GET['babyage'])->whereIn('year',$_GET['year'])->get();
+            }
+
+             //子供の年齢・月を選択のみ選択されている
+             if($_GET['pref'][0] === "都道府県を選ぶ" && $_GET['babyage'][0] !== "年齢を選ぶ" && $_GET['year'][0] === "年を選択" && $_GET['month'][0] !== "月を選択"){
+                $posts=Post::whereIn('babyage_scope_id',$_GET['babyage'])->whereIn('month',$_GET['month'])->get();
+            }
+
+        }else{
+            // ログイン後できた分岐$_GETには何も入ってない
+            $posts=Post::orderBy('created_at','desc')->get();
+        }
+    
+        
+        // if(isset($_GET['pref'],$_GET['babyage'])){
+        //     $posts=Post::where('prefecture_id',$_GET['pref'])->where('babyage',$_GET['babyage'])->get();
+        // }else{
+        //     // ログイン後できた分岐$_GETには何も入ってない
+        //     $posts=Post::orderBy('created_at','desc')->get();
+        // }
+
+
+
+        $user=auth()->user();
+        $prefs=prefecture::all();
+        $ages=babyage_scope::all();
+        // dd($posts[0]->babyage_scope);
+        return view('home', compact('posts', 'user','prefs','ages'));
+        
+    }
+
+    public function mypost() {
+        $user=auth()->user()->id;
+        $posts=Post::where('user_id', $user)->get();
+        return view('mypost', compact('posts'));
+    }
+
+    public function mycomment() {
+        $user=auth()->user()->id;
+        $comments=Comment::where('user_id', $user)->orderBy('created_at', 'desc')->get();
+        return view('mycomment', compact('comments'));
+    }
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,114 @@
+@extends('layouts.app')
+@section('content')
+
+{{-- メッセージ表示 --}}
+@if(session('message'))
+<div class="alert alert-success">{{session('message')}}</div>
+@endif
+
+{{$user->name}}さん
+
+<form method="get" action="" enctype="multipart/form-data">
+    <label for="title">住んでいる地域：</label>
+    <select name="pref[]" class="form-select form-select-sm" aria-label=".form-select-sm example"　size="4" multiple>
+            <option selected>都道府県を選ぶ</option>
+            <!-- ＄prefをforeachで回す -->
+            @foreach($prefs as $pref)
+            <option value="{{$pref['id']}}">{{$pref['name']}}</option>
+            @endforeach
+    </select>
+
+    <label for="title">お子様の年齢：</label>
+    <select name="babyage[]" class="form-select form-select-sm" aria-label=".form-select-sm example" size="4" multiple>
+            <option selected>年齢を選ぶ</option>
+            @foreach($ages as $age)
+            <!-- 表示は０際０ヶ月だけどDB(postsテーブル)には$age['id']でid（文字を入れたくないから）をいれる -->
+            <option value="{{$age['id']}}">{{$age['age']}}</option>
+            @endforeach
+            </select>
+
+    <label for="title">いつの話：</label>
+    <select name="year[]" size="4" multiple>
+            <option>年を選択</option>
+            <option value="2025">2025</option>
+            <option value="2024">2024</option>
+            <option value="2023">2023</option>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+    </select>
+
+    <select name="month[]"　size="4" multiple>
+            <option>月を選択</option>
+            <option value="01">1月</option>
+            <option value="02">2月</option>
+            <option value="03">3月</option>
+            <option value="04">4月</option>
+            <option value="05">5月</option>
+            <option value="06">6月</option>
+            <option value="07">7月</option>
+            <option value="08">8月</option>
+            <option value="09">9月</option>
+            <option value="10">10月</option>
+            <option value="11">11月</option>
+            <option value="12">12月</option>
+    </select>
+
+    <button type="submit" class="btn btn-success">検索</button>
+</form>
+
+
+@foreach ($posts as $post)
+<div class="container-fluid mt-20" style="margin-left:-10px;">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <div class="media flex-wrap w-100 align-items-center">
+                    <img src="{{asset('storage/avatar/'.($post->user->avatar??'user_default.jpg'))}}"
+                        class="rounded-circle" style="width:40px;height:40px;">
+                        <div class="media-body ml-3">
+                       {{$post->user->name??'削除されたユーザ'}}
+                            <div class="text-muted small"> 
+                            地域： {{$post->prefecture->name}}
+                            子供の年齢： {{$post->babyage_scope->age}}
+                            いつの話：{{$post->year}}年
+                            {{$post->month}}月
+                            </div>
+                        </div>
+                        <div class="text-muted small ml-3">
+                            <div>投稿日</div>
+                            <div><strong>{{$post->created_at->diffForHumans()}}</strong> </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <!-- 地域など表示したい -->
+                    <a href="{{route('post.show', $post)}}">
+                    {{ Str::limit ("起床時間:".$post->getup_time."  "."朝食内容:".$post->breakfast."  "."午前の過ごし方:".$post->morning_time."  "."昼食内容:".$post->lunch."  "."午後の過ごし方:".$post->after_time."夕食内容:".$post->dinner."就寝時間:".$post->sleep_time.$post->body, 100, ' ...詳細はこちら') }}
+                    </a>
+                    @if($post->image)
+                    <img src="{{asset('storage/images/'.$post->image)}}" class="img-fluid mx-auto d-block" style="width:200px;">
+                    @endif
+                </div>
+                <div class="card-footer d-flex flex-wrap justify-content-between align-items-center px-0 pt-0 pb-3">
+                    <div class="px-4 pt-3">
+                        @if ($post->comments->count())
+                        <span class="badge badge-success">
+                            コメント{{$post->comments->count()}}件
+                        </span>
+                    @else
+                        <span>コメントはまだありません。</span>
+                    @endif
+                    </div>
+                    <div class="px-4 pt-3"> 
+                       <button type="button" class="btn btn-primary">
+                          <a href="{{route('post.show', $post)}}" style="color:white;">コメントする</a>
+                      </button> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endforeach
+@endsection
